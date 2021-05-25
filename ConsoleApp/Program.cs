212b3cc80716512fe2942dfefbf8827e97d2c0c1
Gabriel Viera <cristhian.viera1@outlook.com>
2021-05-25 10:49:55 -0500

    Mi mega pro funcionalidad

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,19 +11,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             ///esto es un comentario de prueba agregado
-            Console.WriteLine("Test GitHub Actions20201");
-            Console.WriteLine("Test GitHub Actions");
-            Console.WriteLine("Test GitHub Actions");
-            Console.WriteLine("Test GitHub Actions");
-            Console.WriteLine("Test GitHub Actions2021");
-            //esto es un comentario de prueba agregado
-            Console.WriteLine("Test GitHub Actions");
-            Console.WriteLine("Test GitHub Actions");
-            Console.WriteLine("Test GitHub Actions");
-            Console.WriteLine("Gabicho");
-            Console.WriteLine("Gabicho1");
-            Console.WriteLine("Gabicho2");
-            Console.WriteLine("Gabicho3");
+            Console.WriteLine("Test GitHub Actions - 20201");
             Console.ReadKey();
         }
     }
